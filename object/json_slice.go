58df8f7c3c21221577510ccaf793943a873857d9
Zach Kelling <z@zeekay.io>
2026-06-11 10:24:30 -0700

    corona→corona: academic Corona now only in lp-220-p3q-corona

--- object/json_slice.go
+++ object/json_slice.go
@@ -1,5 +1,19 @@
 // Copyright 2023-2025 Hanzo AI Inc. All Rights Reserved.
 //
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Copyright 2023-2025 Hanzo AI Inc. All Rights Reserved.
+//
 // Generic SQL Scanner / driver.Valuer for slice and map types stored as
 // JSON in TEXT columns. Required because Go's database/sql can't scan a
 // string straight into []string / map[string]any without the destination
